4398bfdfafdec21f87057ba9d09119d39c2b065c
m-c-c <14229532902qq.com>
2020-11-19 14:08:13 +0800

    test冲突

--- demo-module1/src/com/mc/Maintest.java
+++ demo-module1/src/com/mc/Maintest.java
@@ -4,7 +4,7 @@ public class Maintest {
     public static void main(String[] args) {
         System.out.println("又一次添加 test ");
 
-        System.out.println("master 冲突");
+        System.out.println("test 冲突");
     }
 
 }
