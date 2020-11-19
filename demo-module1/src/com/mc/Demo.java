043c6aca3ee6390fa5399decae64f44d8d429f21
m-c-c <14229532902qq.com>
2020-11-19 13:55:23 +0800

    更改

--- demo-module1/src/com/mc/Demo.java
+++ demo-module1/src/com/mc/Demo.java
@@ -1,4 +1,6 @@
 package com.mc;
 
 public class Demo {
+
+
 }
---
+++ demo-module1/src/com/mc/Maintest.java
@@ -0,0 +1,8 @@
+package com.mc;
+
+public class Maintest {
+    public static void main(String[] args) {
+        System.out.println("又一次添加 test ");
+    }
+
+}
